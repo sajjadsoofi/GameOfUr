250237b32dd70838faa0a9b4cda5fed983a0a458
sajjad <sajjadsoofi111@gmail.com>
2020-10-05 15:58:53 +0330

    Movement Adjusted

--- Assets/Scripts/PlayerPiece.cs
+++ Assets/Scripts/PlayerPiece.cs
@@ -1,28 +1,70 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class PlayerPiece : MonoBehaviour
 {
     DiceRoller diceRoller;
     public Tile startingTile;
+    public GameObject home;
     Tile currentTile;
+
+    bool scoreMe = false;
+
+    Tile[] moveQueue;
+    int moveQueueIndex;
+
+    Vector2 targetPosition; // Keep track of target position for smooth transition between tiles
+
+
     // Start is called before the first frame update
     void Start()
     {
         diceRoller = GameObject.FindObjectOfType<DiceRoller>();
+
+        targetPosition = this.transform.position;
     }
+
+
+    Vector2 velocity;
+    float smoothTime = 0.2f;
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Vector2.Distance( (Vector2)this.transform.position , targetPosition) < 0.01f )// Remove Hared coded 0.01f
+        {
+            if (moveQueue != null && moveQueueIndex < moveQueue.Length)
+            {
+                Tile nextTile = moveQueue[moveQueueIndex];
+                if (nextTile == null)
+                {
+                    //The piece reached Home
+                    SetNewTargetPosition((Vector2)home.transform.position);
+                }
+                else
+                {
+                    SetNewTargetPosition(nextTile.transform.position);
+                    moveQueueIndex++;
+                }
+                
+            }
+        }
+        // TODO potential performance issue (smoothDamp)
+        this.transform.position = Vector2.SmoothDamp(this.transform.position, targetPosition, ref velocity, smoothTime);
+    }
+
+    void SetNewTargetPosition(Vector2 position) // Helper function for target properties when reseting new target
+    {
+        targetPosition = position;
+        velocity = Vector2.zero;
     }
 
     private void OnMouseUp()
     {
-        //TODO : Resolve UI and GO click conflict.
-        Debug.Log("click");
+        //TODO : Resolve UI and G.O click conflict.
+        //Debug.Log("click");
 
         if (diceRoller.isDoneRolling == false)
         {
@@ -32,31 +74,55 @@ public class PlayerPiece : MonoBehaviour
 
         int spacesToMove = diceRoller.diceTotal;
 
-        Tile finalTile = currentTile; // goal tile . the tile that we are supposed to go 
-                                      // regarding the dice number(spaceToMove)
-                                      //If we roll zero => finalTile = currentTile
+        if (spacesToMove == 0)
+        {
+            return;
+        }
+
+        moveQueue = new Tile[spacesToMove]; //moveQueue will be set as zero to four
+
+        Tile finalTile = currentTile;       // goal tile . the tile that we are supposed to go 
+                                            // regarding the dice number(spaceToMove)
+                                            //If we roll zero => finalTile = currentTile
+        
+        
 
         for (int i = 0; i < spacesToMove; i++) // loops the amount of dice number
         {
-            if (finalTile == null)//if we are not on the board
+            if (finalTile == null && scoreMe == false) //if we are not on the board
             {
                 finalTile = startingTile;
             }
             else
             {
-                finalTile = finalTile.nextTiles[0];
+                if (finalTile.nextTiles == null || finalTile.nextTiles.Length == 0) // Checking if we've
+                {                                                                   //reached the end
+                    //Debug.Log("Score");
+                    //Destroy(gameObject); // We are doing this temporarly
+                    //return;              // TODO : Resolve Reaching the end.
+                    scoreMe = true;
+                    finalTile = null;
+                }
+                else if (finalTile.nextTiles.Length > 1)
+                {
+                    //TODO : Branch based on player Id
+                    finalTile = finalTile.nextTiles[0];
+                }
+                else
+                {
+                    finalTile = finalTile.nextTiles[0];
+                }
             }
-        }
 
-        if (finalTile == null)
-        {
-            return;
+            moveQueue[i] = finalTile; // Generating the move queue
         }
 
         // Teleport the tile to final tile 
-        this.transform.position = finalTile.transform.position;
-        Debug.Log("tile : " + finalTile.transform.position);
-        Debug.Log("piece : " + this.transform.position);
+        //this.transform.position = finalTile.transform.position;
+
+
+        //SetNewTargetPosition(finalTile.transform.position);
+        moveQueueIndex = 0;
         currentTile = finalTile;
     }
 }
